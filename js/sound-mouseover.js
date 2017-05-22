485c38cc603cdfe2155e7bdb89076c6939eee2da
Angel Moran <angel.javier01@gmail.com>
2017-05-22 14:33:35 -0400

    funciona sonido

---
+++ js/sound-mouseover.js
@@ -0,0 +1,13 @@
+
+function playclip() {
+
+  document.all.sound.src = "sonido/beep.mp3";
+
+var audio = document.getElementsByTagName("icono-ubicacion")[0];
+var audio = document.getElementsByTagName("icono-imagenes")[0];
+var audio = document.getElementsByTagName("icono-planos")[0];
+
+
+audio.play();
+
+}
