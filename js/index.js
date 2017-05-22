5b4628feec36b00d702744d9ed073b995d8fef09
angel moran <angel.javier01@gmail.com>
2017-05-21 23:08:24 -0400

    todavia falta en imagenes

--- js/index.js
+++ js/index.js
@@ -8,6 +8,8 @@ $(function(){
     var span_planos = $("#span_planos");
     var item_activo = "";
     var panel1 = $("#panel1");
+    var panel12 = $("#panel12");
+
     var panel2 = $("#panel2");
     var panel3 = $("#panel3");
     var seccion_auxiliar = $("#seccion_auxiliar");
@@ -22,17 +24,61 @@ $(function(){
     var planos = $("#planos li");
     var panel1_logo = $("#panel1_logo");
 
-
-    // eventos
+    var imagenes_play = $("#imagenes_play");
+    var tiempo_ocultar = 1000;  
+    var tiempo_entre_img = 4000;  
+    var tiempo_mostrar = 3000;
+    var tiempo_inicio_anim = 4000;
+    var cambio = $("#cambio");  
+    var cambio1 = $("#cambio1");  
+    var cambio2 = $("#cambio2");  
+    var cambio3 = $("#cambio3");  
+    var cambio4 = $("#cambio4"); 
+    var cambio5 = $("#cambio5");  
+ 
+  
+    
+  
+  
 
     span_ubicacion.hide();
+    span_imagenes.hide();
+    span_planos.hide();
+    seccion_ubicacion1.hide();   
+    seccion_ubicacion2.hide();
+    panel2.hide();
+    panel3.hide();
+    seccion_auxiliar.hide();
+    seccion_ubicacion1.hide();
+    seccion_ubicacion2.hide();
+    seccion_imagenes1.hide();
+    seccion_imagenes2.hide();
+    seccion_imagen_fondo.hide();
+    seccion_planos1.hide();
+    seccion_planos2.hide();
+    cambio1.hide();
+    cambio2.hide();
+    cambio3.hide();
+    cambio4.hide();
+    cambio5.hide();
+
+
+
+
+    // eventos
+  imagenes_play.on("click",function(){
+        cambio.hide();
+        animacion();
+
+
+    });
 
     panel1_logo.on("click",function(){
         ocultarSecciones();
         desactivarIconos();
         panel1.removeClass("active");
-        seccion_auxiliar.removeClass("hidden");
-        seccion_imagen_fondo.removeClass("hidden");
+        seccion_auxiliar.fadeIn();
+        seccion_imagen_fondo.fadeIn();
     });
     
     icono_ubicacion.hover(function(){
@@ -42,7 +88,7 @@ $(function(){
 
     },function(){
         if(item_activo != "ubicacion"){
-            span_ubicacion.fadeOut(500,function(){
+            span_ubicacion.hide(500,function(){
 
             });
         }
@@ -51,17 +97,18 @@ $(function(){
     icono_ubicacion.on("click",function(){
         item_activo = "ubicacion";
         activarIconos();
+
         activarSecciones();
     });
 
     icono_imagenes.hover(function(){
         span_imagenes.fadeIn(200,function(){
-            span_imagenes.removeClass("hidden");
+            span_imagenes.fadeIn();
         });
     },function(){
         if(item_activo != "imagenes"){
-            span_imagenes.fadeOut(500,function(){
-                span_imagenes.addClass("hidden");
+            span_imagenes.hide(500,function(){
+                span_imagenes.hide();
             });
         }
     });
@@ -74,12 +121,12 @@ $(function(){
 
     icono_planos.hover(function(){
         span_planos.fadeIn(200,function(){
-            span_planos.removeClass("hidden");
+            span_planos.fadeIn();
         });
     },function(){
         if(item_activo != "planos"){
-            span_planos.fadeOut(500,function(){
-                span_planos.addClass("hidden");
+            span_planos.hide(500,function(){
+                span_planos.hide();
             });
         }
     });
@@ -93,35 +140,84 @@ $(function(){
     });
 
     planos.on("click",function(){
-        seccion_planos2.removeClass("hidden");
+        seccion_planos2.fadeIn();
     });
 
     // funciones
 
+
+     function animacion() { 
+  
+        // Mostramos la foto 1  
+        cambio1.fadeIn(tiempo_mostrar); 
+ 
+        // Cuando pasen otros 3000 milisegundos, ocultamos la foto 1 y mostramos la foto 2  
+        setTimeout(function() {  
+            // Ocultamos la foto 1  
+
+            cambio1.fadeOut(tiempo_ocultar);  
+            // Mostramos la foto 2  
+            cambio2.fadeIn(tiempo_mostrar);  
+        }, tiempo_entre_img);  
+  
+        // Cuando pasen otros 3000 milisegundos, ocultamos la foto 2 y mostramos la foto 3  
+        setTimeout(function() {  
+            // Ocultamos la foto 2  
+            cambio2.fadeOut(tiempo_ocultar);  
+            // Mostramos la foto 3  
+            cambio3.fadeIn(tiempo_mostrar);  
+        }, tiempo_entre_img * 2); 
+
+        setTimeout(function() {  
+            // Ocultamos la foto 3  
+            cambio3.fadeOut(tiempo_ocultar);  
+            // Mostramos la foto 4  
+            cambio4.fadeIn(tiempo_mostrar);  
+        }, tiempo_entre_img * 3); 
+
+         setTimeout(function() {  
+            // Ocultamos la foto 4  
+            cambio4.fadeOut(tiempo_ocultar);  
+            // Mostramos la foto 5  
+            cambio5.fadeIn(tiempo_mostrar);  
+        }, tiempo_entre_img * 4);   
+  
+        // Cuando pasen otros 3000 milisegundos, ocultamos la foto 3 y volvemos a iniciar la animación  
+        setTimeout(function() {  
+            // Ocultamos la foto 3  
+            $cambio5.fadeOut(tiempo_ocultar);  
+            // Iniciamos otra vez la animación  
+            animacion();  
+        }, tiempo_inicio_anim); 
+
+        
+    } 
+
     function ocultarSecciones(){
-        seccion_auxiliar.addClass("hidden");
-        seccion_ubicacion1.addClass("hidden");
-        seccion_ubicacion2.addClass("hidden");
-        seccion_imagenes1.addClass("hidden");
-        seccion_imagenes2.addClass("hidden");
-        seccion_planos1.addClass("hidden");
-        seccion_planos2.addClass("hidden");
-        seccion_imagen_fondo.addClass("hidden");
+seccion_ubicacion1.hide();   
+     seccion_ubicacion2.hide();
+        seccion_auxiliar.hide();
+        seccion_imagenes1.hide();
+        seccion_imagenes2.hide();
+        seccion_planos1.hide();
+        seccion_planos2.hide();
+        seccion_imagen_fondo.hide();
     }
 
     function activarSecciones(){
         ocultarSecciones();
         switch(item_activo){
             case "ubicacion":
-                seccion_ubicacion1.removeClass("hidden");
-                seccion_ubicacion2.removeClass("hidden");
-                break;
+         
+            seccion_ubicacion1.fadeIn();
+              seccion_ubicacion2.fadeIn("slow");
+              break;
             case "imagenes":
-                seccion_imagenes1.removeClass("hidden");
-                seccion_imagenes2.removeClass("hidden");
+                seccion_imagenes1.fadeIn();
+                seccion_imagenes2.fadeIn();
                 break;
             case "planos":
-                seccion_planos1.removeClass("hidden");
+                seccion_planos1.fadeIn();
                 break;
         }
     }
@@ -132,15 +228,16 @@ $(function(){
         switch(item_activo){
             case "ubicacion":
                 icono_ubicacion.addClass("active");
-                span_ubicacion.removeClass("hidden");
+                span_ubicacion.fadeIn();
+
                 break;
             case "imagenes":
                 icono_imagenes.addClass("active");
-                span_imagenes.removeClass("hidden");
+                span_imagenes.fadeIn();
                 break;
             case "planos":
                 icono_planos.addClass("active")
-                span_planos.removeClass("hidden");
+                span_planos.fadeIn();
                 break;
         }
     }
@@ -150,9 +247,9 @@ $(function(){
         icono_imagenes.removeClass("active");
         icono_planos.removeClass("active");
 
-        span_ubicacion.addClass("hidden");
-        span_imagenes.addClass("hidden");
-        span_planos.addClass("hidden");
+        span_ubicacion.hide();
+        span_imagenes.hide();
+        span_planos.hide();
     }
 
     function activarPanel1(){
